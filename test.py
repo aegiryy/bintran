13f3bcb376f517e43951ac6048dc275f884decfb
Xinyang Ge <aegiryy@gmail.com>
2015-01-22 00:30:07 -0500

    rewrite call_to_jmp and add_bound_check

--- test.py
+++ test.py
@@ -6,16 +6,6 @@ from bintran import Elf32, Elf32_Rel, Elf32_Sym, Elf32_Shdr
 from uuid import uuid4
 from ctypes import *
 
-def flatten(elf):
-    '''convert all short JMPs to near JMPs'''
-    sjs = filter(lambda i: i.mnemonic.startswith('j') and \
-            i.op_str[0] != '*' and len(i) == 2, elf.disasm())
-    print '  %d short JMPs' % len(sjs)
-    if not sjs:
-        return elf
-    elf.flatten()
-    return elf
-
 def cfi_2(elf):
     '''fill ret_table'''
     assert elf.ehdr.e_type == 2, 'not an executable?'
@@ -71,64 +61,36 @@ def cfi_1(elf):
                 '\x59\xff\x24\x8d\xef\xbe\xad\xde' # "pop %ecx; jmp *0xdeadbeef(,%ecx,4)"
     return elf
 
-def call_to_jmp(elf):
-    '''convert CALL to PUSH and JMP'''
-    syms = elf('.symtab', Elf32_Sym)
-    for ndx in range(len(syms)):
-        if syms[ndx].st_info & 0xf == 3 and syms[ndx].st_shndx == 1:
-            break # found symbol of STT_SECTION for .text
-    else:
-        assert False, 'no symbol for .text section'
-    calls = filter(lambda i: i.mnemonic == 'call', elf.disasm())
-    print '  %d CALLs found' % len(calls)
-    calls.reverse()
-    for i in calls:
-        r = Elf32_Rel(i.address+1, (ndx<<8)+1)
-        push = '\x68%s' % string_at(pointer(c_uint(i.address+5+len(i))), 4)
-        elf.insert(i.address, push)
-        if not elf('.rel.text'):
-            elf.add_section('.rel.text', sh_type = 9, sh_info = 1, sh_entsize = sizeof(r),
-                    sh_link = (addressof(elf('.symtab')) - addressof(elf.shdrs)) / sizeof(Elf32_Shdr))
-        elf.add_entry(elf('.rel.text'), r, lambda r: r.r_offset)
-        text_offset = elf('.text').sh_offset
-        if elf[text_offset+i.address+len(push)] == '\xe8': # direct CALL
-            elf[text_offset+i.address+len(push)] = '\xe9'
-        elif elf[text_offset+i.address+len(push)] == '\xff': # indirect CALL
-            elf[text_offset+i.address+len(push)+1] = chr(ord(elf[text_offset+i.address+len(push)+1]) + 0x10)
-        else:
-            assert False, 'unexpected CALL? %s %s' % (i.mnemonic, i.op_str)
+def flatten(elf):
+    '''rewrite short "jmp" to near "jmp"'''
+    sjs = filter(lambda i: i.mnemonic.startswith('j') and \
+            i.op_str[0] != '*' and len(i) == 2, elf.disasm())
+    print '  %d short JMPs' % len(sjs)
+    elf.flatten()
     return elf
 
-def protect_switch(elf):
-    '''add checks before jmp *table(,%reg,4)'''
+def add_bound_check(elf):
+    '''add checks before "jmp/call *table(,%reg,4)"'''
     syms = elf('.symtab', Elf32_Sym)
     insns = elf.disasm()
     insns.reverse()
     for i in insns:
-        if i.mnemonic != 'jmp':
-            continue
+        # parse the instruction
         r = re.search(r'\*(.*)\(,%(.*),4\)', i.op_str)
         if not r:
             continue
-        print '  %x: %s %s' % (i.address, i.mnemonic, i.op_str)
+        print '  %s' % str(i)
         addend, reg = int(r.group(1), 16), r.group(2)
-        assert addend == 0, 'more than two switchs?'
         # find the relent of table base in the instruction
-        for r in elf('.rel.text', Elf32_Rel):
-            if r.r_offset == i.address + 3:
-                break
-        else:
-            assert False, 'relent for switch-case jump table is not found'
+        r = next((r for r in elf('.rel.text', Elf32_Rel) if r.r_offset == i.address + 3), None)
+        assert r, 'relent for jump table is not found'
         # find the symbol associated with the relent
         assert r.r_info & 0xff == 1, 'not an R_386_32 relent?'
         s = syms[r.r_info>>8]
         assert s.st_info & 0xf == 3, 'not a section symbol?'
         # find SHT_REL section for jump table section (e.g., .rodata)
-        for sh in elf.shdrs:
-            if sh.sh_type == 9 and sh.sh_info == s.st_shndx:
-                break
-        else:
-            assert False, 'SHT_REL for jump table section not found'
+        sh = next((sh for sh in elf.shdrs if sh.sh_type == 9 and sh.sh_info == s.st_shndx), None)
+        assert sh, 'SHT_REL for jump table section is not found'
         # count the number of jump table entries
         rels = (sh.sh_size/sizeof(Elf32_Rel) * Elf32_Rel).from_buffer(elf.binary, sh.sh_offset)
         count = 0
@@ -142,7 +104,8 @@ def protect_switch(elf):
             else:
                 break
         # generate instrumentation
-        nasm = 'bits 32\ncmp %s,%d\nspin: jae spin' % (reg, count)
+        nasm = ('cmp %s,%d\n'
+                'jae short $\n') % (reg, count)
         tmpfile = '.%s' % uuid4()
         with open(tmpfile, 'w') as f:
             f.write(nasm)
@@ -155,8 +118,35 @@ def protect_switch(elf):
         elf.insert(i.address, payload)
     return elf
 
+def call_to_jmp(elf):
+    '''rewrite "call dest" to "push addr; jmp dest"'''
+    syms = elf('.symtab', Elf32_Sym)
+    # find symbol index of .text section
+    tsndx = next((i for i in range(len(syms)) if syms[i].st_info & 0xf == 3 and syms[i].st_shndx == 1), -1)
+    assert tsndx >= 0, 'symbol of .text section is not found'
+    calls = filter(lambda i: i.mnemonic == 'call', elf.disasm())
+    print '  %d CALLs found' % len(calls)
+    calls.reverse()
+    for i in calls:
+        # insert "push addr"
+        elf.insert(i.address, '\x68%s' % string_at(pointer(c_uint(i.address+5+len(i))), 4))
+        # add a relent for the return address in "push"
+        if not elf('.rel.text'):
+            symtab_shndx = (addressof(elf('.symtab'))-addressof(elf.shdrs)) / sizeof(Elf32_Shdr)
+            elf.add_section('.rel.text', sh_type=9, sh_info=1, sh_entsize=sizeof(Elf32_Rel), sh_link=symtab_shndx)
+        elf.add_entry(elf('.rel.text'), Elf32_Rel(i.address+1, (tsndx<<8)+1), lambda r: r.r_offset)
+        # rewrite "call" to "jmp"
+        call_offset = elf('.text').sh_offset + i.address + 5
+        if elf[call_offset] == '\xe8': # direct "call"
+            elf[call_offset] = '\xe9'
+        elif elf[call_offset] == '\xff': # indirect "call"
+            elf[call_offset+1] = chr(ord(elf[call_offset+1]) + 0x10)
+        else:
+            assert False, 'what is the call? %s' % str(i)
+    return elf
+
 def ret_to_jmp(elf):
-    '''rewrite "ret" with "pop %ecx; jmp *%ecx"'''
+    '''rewrite "ret" to "pop %ecx; jmp *%ecx"'''
     rets = [i.address for i in filter(lambda i: i.bytes == '\xc3', elf.disasm())]
     print '  %d RETs found' % len(rets)
     rets.reverse()
@@ -178,8 +168,8 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         print ('usage: test.py [option+option+..] xxx.o\n'
                '  flatten: rewrite all short JMPs to near JMPs\n'
+               '  add_bound_check: add check before jump table indexing\n'
                '  call_to_jmp: rewrite CALL to PUSH and JMP\n'
-               '  protect_switch: add check before jump table indexing\n'
                '  ret_to_jmp: rewrite RET to POP and JMP\n'
                '  add_nop: add NOP after every instruction')
         sys.exit(0)
